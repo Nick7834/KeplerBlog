eacbfc0360b6dddc397e5e87dedfb97608720341
Никита <history4527@gmail.com>
2025-02-15 17:23:56 +0300

    -v 2.1.12

--- src/app/api/comments/[id]/comment/route.ts
+++ src/app/api/comments/[id]/comment/route.ts
@@ -80,7 +80,6 @@ export async function GET(request: Request, { params }: { params: Promise<{ id: 
       },
     });
 
-
     return NextResponse.json(comments);
   } catch (error) {
     console.error('Error fetching comments:', error);
