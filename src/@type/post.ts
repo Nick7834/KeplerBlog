c716ae2e2d9465890034337e924db0446da586f4
Никита <history4527@gmail.com>
2025-03-01 16:35:46 +0300

    -v 2.1.38

---
+++ src/@type/post.ts
@@ -0,0 +1,36 @@
+import { JsonValue } from "@prisma/client/runtime/library";
+import { RawDraftContentState } from "draft-js";
+
+export interface IPost {
+    createdAt: Date;
+    id: string;
+    title: string;
+    content?: RawDraftContentState | JsonValue;
+    image?: string[];
+    author: {
+        id: string;
+        username: string;
+        profileImage: string | null;
+        isverified: boolean;
+    };
+    comments: {
+        id: string;
+        content: string;
+        createdAt: Date;
+        author: {
+            id: string;
+            username: string;
+            profileImage: string | null;
+            isverified: boolean;
+        }
+    }[]; 
+    isLiked: boolean;
+    isFollowing: boolean;
+    likes: {
+        id: string;
+    }[];
+    _count: {
+        comments: number;
+        likes: number;
+    };
+}
