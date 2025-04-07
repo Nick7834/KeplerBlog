42e7139f51a4a2d88c69f98f5db7580484f03d8b
Никита <history4527@gmail.com>
2025-04-07 12:51:33 +0300

    -v 2.7.42

--- src/components/hooks/useScrollToTop.ts
+++ src/components/hooks/useScrollToTop.ts
@@ -2,16 +2,27 @@ import { IPost } from "@/@types/post";
 import { useEffect, useRef } from "react";
 
 const useScrollToTop = (isLoading: boolean, posts: IPost[]) => {
-    const hasScrolled = useRef(false); 
-    
+    const hasScrolled = useRef(false);
+    const isPageReloaded = useRef(false); 
+  
     useEffect(() => {
-        if (isLoading || hasScrolled.current) return;
- 
+
+      if (sessionStorage.getItem("pageReloaded")) {
+        isPageReloaded.current = true;
+      } else {
+        sessionStorage.setItem("pageReloaded", "true"); 
+      }
+  
+      if (isLoading || hasScrolled.current || posts.length > 0) return; 
+
+      if (isPageReloaded.current) {
         setTimeout(() => {
           window.scrollTo({ top: 0, behavior: "instant" });
           hasScrolled.current = true; 
-        }, 100); 
-      }, [isLoading, posts]);  
+          sessionStorage.removeItem("pageReloaded"); 
+        }, 100);
+      }
+    }, [isLoading, posts]); 
 };
 
 export default useScrollToTop;
