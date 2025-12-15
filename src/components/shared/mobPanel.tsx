41b4f2a0e52f8c0d5758d5c1ea38b68e0f0e4a7b
Никита <history4527@gmail.com>
2025-12-15 16:37:47 +0300

    -v 3.4.9.3 -test

--- src/components/shared/mobPanel.tsx
+++ src/components/shared/mobPanel.tsx
@@ -1,73 +1,100 @@
-'use client'
-import { cn } from '@/lib/utils';
-import React from 'react';
-import { HiTrendingUp } from 'react-icons/hi';
-import { IoHome } from 'react-icons/io5';
-import { MdPeopleAlt } from 'react-icons/md';
+"use client";
+import { cn } from "@/lib/utils";
+import React from "react";
+import { HiTrendingUp } from "react-icons/hi";
+import { IoHome } from "react-icons/io5";
+import { MdPeopleAlt } from "react-icons/md";
 import { IoAddOutline } from "react-icons/io5";
-import { FaNewspaper } from 'react-icons/fa6';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { useSession } from 'next-auth/react';
+import { FaNewspaper } from "react-icons/fa6";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 interface Props {
-    className?: string;
-} 
+  className?: string;
+}
 
 export const MobPanel: React.FC<Props> = ({ className }) => {
+  const namePage = usePathname();
 
-     const namePage = usePathname();
+  const { data: session } = useSession();
 
-     const { data: session } = useSession();
+  const navMenuMob = [
+    {
+      name: "Home",
+      svg: <IoHome />,
+      href: "/",
+    },
+    {
+      name: "Trending",
+      svg: <HiTrendingUp />,
+      href: "/trending",
+    },
+    ...(session
+      ? [
+          {
+            name: "Create",
+            svg: <IoAddOutline />,
+            href: "/create",
+          },
+        ]
+      : []),
+    {
+      name: "For You",
+      svg: <FaNewspaper />,
+      href: "/for-you",
+    },
+    {
+      name: "Subs",
+      svg: <MdPeopleAlt />,
+      href: "/subscriptions",
+    },
+  ];
 
-     console.log(namePage)
+  const cleanPath = namePage?.replace(/\/$/, "");
 
-    const navMenuMob = [
-            {
-                name: "Home",
-                svg: <IoHome />,
-                href: "/"
-            }, 
-            {
-                name: "Trending",
-                svg: <HiTrendingUp />,
-                href: "/trending"
-            },
-            ...(session ? [{
-                name: "Create",
-                svg: <IoAddOutline />,
-                href: "/create"
-            }] : []),
-            {
-                name: "For You",
-                svg: <FaNewspaper />,
-                href: "/for-you"
-            },
-            {
-                name: "Subs",
-                svg: <MdPeopleAlt />,
-                href: "/subscriptions"
-            }
-    ]
-
-     console.log(namePage === navMenuMob[0].href)
-
-    return (
-        <div className={cn('mob-panel hidden fixed bottom-0 left-0 w-full z-[500]', className)}>
-            <nav className='m-[5px] rounded-[20px] py-2 px-1 bg-[#E0E0E0]/70 dark:bg-[#2a2a2a]/80 backdrop-blur-[12px] border border-solid border-[#b0b0b0]/20 dark:border-[#d9d9d9]/20'>
-                <ul className={cn('grid grid-cols-5 items-center justify-center h-full gap-1', !session && 'grid-cols-4')}>
-                    {navMenuMob.map((item, index) => ( 
-                        <li key={index} 
-                             className='flex flex-col items-center justify-center gap-1 w-full h-full'>
-                             <Link href={item.href} className={cn('flex flex-col gap-1 items-center justify-center w-full h-full text-[#333333] dark:text-[#d9d9d9] text-sm font-bold', namePage === item.href && 'dark:text-[#7391d5] text-[#7391d5]')}>
-                                <span className={cn('text-[#333333] dark:text-[#d9d9d9] text-[24px] font-bold block', namePage === item.href && 'dark:text-[#7391d5] text-[#7391d5]')}>{item.svg}</span>
-                                <span className='block mt-auto leading-3'>{item.name}</span>
-                            </Link>
-                        </li>
-                    ))}
-
-                </ul>
-            </nav>
-        </div>
-    );
+  return (
+    <div
+      className={cn(
+        "mob-panel hidden fixed bottom-0 left-0 w-full z-[500]",
+        className
+      )}
+    >
+      <nav className="m-[5px] rounded-[20px] py-2 px-1 bg-[#E0E0E0]/70 dark:bg-[#2a2a2a]/80 backdrop-blur-[12px] border border-solid border-[#b0b0b0]/20 dark:border-[#d9d9d9]/20">
+        <ul
+          className={cn(
+            "grid grid-cols-5 items-center justify-center h-full gap-1",
+            !session && "grid-cols-4"
+          )}
+        >
+          {navMenuMob.map((item, index) => (
+            <li
+              key={index}
+              className="flex flex-col items-center justify-center gap-1 w-full h-full"
+            >
+              <Link
+                href={item.href}
+                className={cn(
+                  "flex flex-col gap-1 items-center justify-center w-full h-full text-[#333333] dark:text-[#d9d9d9] text-sm font-bold",
+                  cleanPath === item.href.replace(/\/$/, "") &&
+                    "dark:text-[#7391d5] text-[#7391d5]"
+                )}
+              >
+                <span
+                  className={cn(
+                    "text-[#333333] dark:text-[#d9d9d9] text-[24px] font-bold block",
+                    cleanPath === item.href.replace(/\/$/, "") &&
+                      "dark:text-[#7391d5] text-[#7391d5]"
+                  )}
+                >
+                  {item.svg}
+                </span>
+                <span className="block mt-auto leading-3">{item.name}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
 };
