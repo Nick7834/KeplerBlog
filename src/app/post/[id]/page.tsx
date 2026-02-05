2c3b6beedac75ead7c50b55b627e3acc376934c6
Nick7834 <Hellreg124@gmail.com>
2026-02-05 16:55:13 +0300

    -v 3.4.66

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,18 +1,21 @@
 import { PostDetails } from "@/components/shared/postDetailsId";
 import { redirect } from "next/navigation";
 
-export default async function PostDetail({ params }: { params: Promise<{ id: string }> }) {
+export default async function PostDetail({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id: id } = await params;
 
-    const { id: id } = await params;
+  if (!id) redirect("/");
 
-    if(!id) redirect('/');
+  const isValidObjectId = /^[a-f\d]{24}$/i.test(id);
+  if (!isValidObjectId) redirect("/");
 
-    const isValidObjectId = /^[a-f\d]{24}$/i.test(id);
-    if (!isValidObjectId) redirect('/');
-
-    return (
-        <div>
-           <PostDetails idPost={id}  />
-        </div>
-    );
+  return (
+    <div>
+      <PostDetails idPost={id} />
+    </div>
+  );
 }
