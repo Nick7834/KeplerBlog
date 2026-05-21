350255c805d191f09cec839626cb0e2e687ac7f3
Nick7834 <Hellreg124@gmail.com>
2026-05-21 14:37:38 +0300

    -v 3.5.4

--- src/components/shared/notificationComponent.tsx
+++ src/components/shared/notificationComponent.tsx
@@ -83,124 +83,139 @@ export const NotificationComponent: React.FC<Props> = ({ session }) => {
           id="scrollable-container"
           className="relative h-[400px] max-[1100px]:h-[92%] overflow-auto scrollbar"
         >
-          <InfiniteScroll
-            dataLength={notificationsData.length}
-            next={loadMoreNotifications}
-            hasMore={hasMore}
-            loader={
-              <div
-                className={cn(
-                  "absolute top-0 left-0 w-full flex justify-center items-center h-full",
-                  notificationsData.length >= 10 ? "static p-2" : "",
-                )}
-              >
-                <Oval
-                  visible={true}
-                  height="50"
-                  width="50"
-                  color="#7391d5"
-                  secondaryColor="#7391d5"
-                  ariaLabel="oval-loading"
-                  strokeWidth={4}
-                />
-              </div>
-            }
-            scrollableTarget="scrollable-container"
-            className={cn(
-              "flex flex-col gap-1 h-full",
-              width < 1100 ? "pb-[10px]" : "",
-            )}
-          >
-            {!isLoading && notificationsData.length === 0 ?
-              <div className="absolute top-0 left-0 w-full flex flex-col items-center justify-center gap-2 h-full">
-                <BiSolidNotificationOff size={70} />
-                <p className="text-[#333333] dark:text-[#d9d9d9] text-lg font-bold">
-                  No notifications
-                </p>
-              </div>
-            : notificationsData.map(
-                (notification: NotificationDataType, index: number) => (
-                  <Link
-                    onClick={() => (
-                      handleClickNotification(
-                        notification.id,
-                        notification.isRead,
-                      ),
-                      setIsOpen(!isOpen)
-                    )}
-                    key={index}
-                    href={`/post/${notification.postId}`}
-                    className="relative flex items-center justify-between gap-2 hover:bg-[#d1d1d1]/60 hover:dark:bg-[#333333]/60 
+          {isLoading && notificationsData.length === 0 && (
+            <div className="absolute top-0 left-0 w-full flex justify-center items-center h-full">
+              <Oval
+                visible={true}
+                height="50"
+                width="50"
+                color="#7391d5"
+                secondaryColor="#7391d5"
+                ariaLabel="oval-loading"
+                strokeWidth={4}
+              />
+            </div>
+          )}
+          {!isLoading && (
+            <InfiniteScroll
+              dataLength={notificationsData.length}
+              next={loadMoreNotifications}
+              hasMore={hasMore}
+              loader={
+                <div
+                  className={cn(
+                    "absolute top-0 left-0 w-full flex justify-center items-center h-full",
+                    notificationsData.length >= 10 ? "static p-2" : "",
+                  )}
+                >
+                  <Oval
+                    visible={true}
+                    height="50"
+                    width="50"
+                    color="#7391d5"
+                    secondaryColor="#7391d5"
+                    ariaLabel="oval-loading"
+                    strokeWidth={4}
+                  />
+                </div>
+              }
+              scrollableTarget="scrollable-container"
+              className={cn(
+                "flex flex-col gap-1 h-full",
+                width < 1100 ? "pb-[10px]" : "",
+              )}
+            >
+              {!isLoading && notificationsData.length === 0 ?
+                <div className="absolute top-0 left-0 w-full flex flex-col items-center justify-center gap-2 h-full">
+                  <BiSolidNotificationOff size={70} />
+                  <p className="text-[#333333] dark:text-[#d9d9d9] text-lg font-bold">
+                    No notifications
+                  </p>
+                </div>
+              : notificationsData.map(
+                  (notification: NotificationDataType, index: number) => (
+                    <Link
+                      onClick={() => (
+                        handleClickNotification(
+                          notification.id,
+                          notification.isRead,
+                        ),
+                        setIsOpen(!isOpen)
+                      )}
+                      key={index}
+                      href={`/post/${notification.postId}`}
+                      className="relative flex items-center justify-between gap-2 hover:bg-[#d1d1d1]/60 hover:dark:bg-[#333333]/60 
                                     rounded-[10px] py-3 px-[clamp(0.875rem,0.721rem+0.77vw,1.25rem)]"
-                  >
-                    <div className="flex items-center justify-between w-full gap-2">
-                      <div className="flex items-center gap-2">
-                        {!notification.isRead && (
-                          <span className="absolute left-[clamp(0.156rem,0.092rem+0.32vw,0.313rem)] block w-fit min-w-2 min-h-2 bg-[#7391d5] dark:bg-[#7391d5] rounded-full"></span>
-                        )}
+                    >
+                      <div className="flex items-center justify-between w-full gap-2">
+                        <div className="flex items-center gap-2">
+                          {!notification.isRead && (
+                            <span className="absolute left-[clamp(0.156rem,0.092rem+0.32vw,0.313rem)] block w-fit min-w-2 min-h-2 bg-[#7391d5] dark:bg-[#7391d5] rounded-full"></span>
+                          )}
 
-                        <div className="flex items-center gap-3">
-                          {(
-                            notification.sender &&
-                            notification.sender.profileImage
-                          ) ?
-                            <img
-                              src={notification.sender.profileImage}
-                              alt="avatar"
-                              className="min-w-[50px] max-w-[50px] w-full min-h-[50px] max-h-[50px] h-full object-cover rounded-full"
-                            />
-                          : <span className="flex flex-col items-center justify-center rounded-full min-w-[50px] max-w-[50px] w-full min-h-[50px] max-h-[50px] h-full bg-[#c7c7c7]">
-                              <FaRegUser className="text-[#333333]" />
-                            </span>
-                          }
-                          <div className="flex flex-col gap-1">
-                            <p className="text-[#333333] dark:text-[#d9d9d9] text-sm font-medium">
-                              {width >= 720 ?
-                                notification.message.length > 150 ?
+                          <div className="flex items-center gap-3">
+                            {(
+                              notification.sender &&
+                              notification.sender.profileImage
+                            ) ?
+                              <img
+                                src={notification.sender.profileImage}
+                                alt="avatar"
+                                className="min-w-[50px] max-w-[50px] w-full min-h-[50px] max-h-[50px] h-full object-cover rounded-full"
+                              />
+                            : <span className="flex flex-col items-center justify-center rounded-full min-w-[50px] max-w-[50px] w-full min-h-[50px] max-h-[50px] h-full bg-[#c7c7c7]">
+                                <FaRegUser className="text-[#333333]" />
+                              </span>
+                            }
+                            <div className="flex flex-col gap-1">
+                              <p className="text-[#333333] dark:text-[#d9d9d9] text-sm font-medium">
+                                {width >= 720 ?
+                                  notification.message.length > 150 ?
+                                    `${notification.message
+                                      .substring(0, 150)
+                                      .trim()}...`
+                                  : notification.message
+                                : notification.message.length > 75 ?
                                   `${notification.message
-                                    .substring(0, 150)
+                                    .substring(0, 110)
                                     .trim()}...`
-                                : notification.message
-                              : notification.message.length > 75 ?
-                                `${notification.message
-                                  .substring(0, 110)
-                                  .trim()}...`
-                              : notification.message}
-                            </p>
-                            <span className="text-[#6a6a6a] dark:text-[#cecece] text-xs">
-                              {notification.createdAt &&
-                                getShortTimeAgo(
-                                  new Date(notification.createdAt),
-                                )}
-                            </span>
+                                : notification.message}
+                              </p>
+                              <span className="text-[#6a6a6a] dark:text-[#cecece] text-xs">
+                                {notification.createdAt &&
+                                  getShortTimeAgo(
+                                    new Date(notification.createdAt),
+                                  )}
+                              </span>
+                            </div>
                           </div>
                         </div>
+                        {notification.postImage && (
+                          <img
+                            src={notification.postImage}
+                            alt="arrow"
+                            className="min-w-[100px] max-w-[100px] max-h-[70px] h-full object-cover rounded-[10px]"
+                          />
+                        )}
                       </div>
-                      {notification.postImage && (
-                        <img
-                          src={notification.postImage}
-                          alt="arrow"
-                          className="min-w-[100px] max-w-[100px] max-h-[70px] h-full object-cover rounded-[10px]"
-                        />
-                      )}
-                    </div>
 
-                    <Button
-                      onClick={(e) => (
-                        e.stopPropagation(),
-                        e.preventDefault(),
-                        handleDel(notification.id)
-                      )}
-                      variant="outline"
-                      className="p-0 w-fit h-full bg-transparent border-0 hover:bg-transparent"
-                    >
-                      <MdVisibilityOff />
-                    </Button>
-                  </Link>
-                ),
-              )
-            }
-          </InfiniteScroll>
+                      <Button
+                        onClick={(e) => (
+                          e.stopPropagation(),
+                          e.preventDefault(),
+                          handleDel(notification.id)
+                        )}
+                        variant="outline"
+                        className="p-0 w-fit h-full bg-transparent border-0 hover:bg-transparent"
+                      >
+                        <MdVisibilityOff />
+                      </Button>
+                    </Link>
+                  ),
+                )
+              }
+            </InfiniteScroll>
+          )}
         </div>
 
         <Button
